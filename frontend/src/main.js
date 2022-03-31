7b2a92869dcf7b67356127136f8c66bc5ff52e8a
t-kehiguchi <t0tp50gxgpb8ibksqp9r@outlook.com>
2022-04-01 00:00:36 +0900

    Bootstrap導入、vue-routerのバージョンダウン(routerをimportできるように修正)、vue-session導入

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
+import router from './router';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import HelloWorld from '@/components/HelloWorld';
 
 Vue.config.productionTip = false;
 Vue.prototype.$axios = axios;
 
 new Vue({
+  router,
   render: h => h(HelloWorld),
 }).$mount('#app');
